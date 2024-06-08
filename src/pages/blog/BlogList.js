b1e525625548d4deef37a016977cf91e5df09a6f
YeaminRabbi <yeaminrabbi308@gmail.com>
2024-06-08 18:38:53 +0600

    fetch data from DB.JSON

--- src/pages/blog/BlogList.js
+++ src/pages/blog/BlogList.js
@@ -1,4 +1,20 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+// const BlogList = ({ blogs, title, handleDelete= null }) => {
+//     return (
+//       <div className="blog-list">
+//         <h2>{ title }</h2>
+//         {blogs.map(blog => (
+//           <div className="blog-preview" key={blog.id} >
+//             <h2>{ blog.title }</h2>
+//             <p>Written by { blog.author }</p>
+//             <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+//           </div>
+//         ))}
+//       </div>
+//     );
+//   }
+   
+//   export default BlogList;
+const BlogList = ({ blogs, title }) => {
     return (
       <div className="blog-list">
         <h2>{ title }</h2>
@@ -6,7 +22,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
           <div className="blog-preview" key={blog.id} >
             <h2>{ blog.title }</h2>
             <p>Written by { blog.author }</p>
-            <button onClick={() => handleDelete(blog.id)}>delete blog</button>
           </div>
         ))}
       </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import BlogList from "../blog/BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+//   const [blogs, setBlogs] = useState([
+//     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+//     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+//     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+//   ])
+  const [blogs, setBlogs] = useState(null)
 
   const [name, setName] = useState('Jellyfish');
 
@@ -15,16 +16,24 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
+
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-  }, [name])
+    fetch('http://localhost:8000/blogs')
+        .then(response => {
+            return response.json();
+        })
+        .then((data)=>{
+            setBlogs(data)
+        })
+
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('Tushar')}>change name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
+      {/* {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />} */}
+      {/* <button onClick={() => setName('Tushar')}>change name</button>
+      <p>{name}</p> */}
     </div>
   );
 }
