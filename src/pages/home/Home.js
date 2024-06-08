5a30457d43e1ab0d19c4a70d586ebba501f372ca
YeaminRabbi <yeaminrabbi308@gmail.com>
2024-06-08 18:57:35 +0600

    error handling && loading effect from reponse

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,32 +8,36 @@ const Home = () => {
 //     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
 //   ])
   const [blogs, setBlogs] = useState(null)
-
-  const [name, setName] = useState('Jellyfish');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-        .then(response => {
-            return response.json();
-        })
-        .then((data)=>{
-            setBlogs(data)
-        })
+    setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(response => {
+                if(!response.ok){
+                   throw Error('Could not fetch the data for that resource');
+                }
+                return response.json();
+            })
+            .then((data)=>{
+                setBlogs(data)
+                setIsPending(false)
+            })
+            .catch((error) => {
+                setError(error.message)
+                setIsPending(false)
 
-  }, [])
+            })
+    }, 1000)
+
+}, [])
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
-      {/* {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />} */}
-      {/* <button onClick={() => setName('Tushar')}>change name</button>
-      <p>{name}</p> */}
+        {error && <div>{error}</div>}
+        {isPending && <div> Loading... </div> }
+        {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
     </div>
   );
 }
