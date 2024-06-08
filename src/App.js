f88b05f60ae58084b8102ba724069aa5a534e9a7
YeaminRabbi <yeaminrabbi308@gmail.com>
2024-06-08 17:55:43 +0600

    basic Blog List View

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import Navbar from './pages/navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-        <div className="content">
-            <h1>App Component</h1>
-        </div>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ src/pages/navbar/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <h1>The Jelly Blog</h1>
+        <div className="links">
+          <a href="/">Home |</a>
+          <a href="/">About</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
