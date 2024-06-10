127e26b6523e91a3b967b1f3b5370eb183a03b8f
YeaminRabbi <yeaminrabbi308@gmail.com>
2024-06-10 16:39:54 +0600

    CRUD operation implemented | project completion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+
 import Navbar from './pages/navbar/Navbar';
 import Home from './pages/home/Home';
+import NotFound from './pages/others/NotFound.js';
+import Create from './pages/blog/Create.js';
+import BlogDetails from './pages/blog/BlogDetails.js';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />  
+            <Route path="/create" element={<Create />} /> 
+            <Route path="/blogs/:id" element={<BlogDetails />} /> 
+            <Route path="*" element={<NotFound />} /> 
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
